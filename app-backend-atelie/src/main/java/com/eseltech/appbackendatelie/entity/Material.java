b879ff1d0f20fd9fdb4f09494b2933077be75635
flaviachristine <flavia.pereira@sptech.school>
2026-05-29 18:31:26 -0300

    fix/Correçã de erros

--- app-backend-atelie/src/main/java/com/eseltech/appbackendatelie/entity/Material.java
+++ app-backend-atelie/src/main/java/com/eseltech/appbackendatelie/entity/Material.java
@@ -18,7 +18,7 @@ public class Material {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     @Schema(description = "Identificador único do material", example = "1", accessMode = Schema.AccessMode.READ_ONLY)
-    private Long id;
+    private Integer id;
 
     @ManyToOne
     private Empresa empresa;
@@ -61,11 +61,11 @@ public class Material {
         this.preco = preco;
     }
 
-    public Long getId() {
+    public Integer getId() {
         return id;
     }
 
-    public void setId(Long id) {
+    public void setId(Integer id) {
         this.id = id;
     }
 
